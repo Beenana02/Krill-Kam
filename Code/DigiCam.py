80f2ffa25b994027b57c0e99277d7e55a5ff446e
Beenana02 <ghabeenana@gmail.com>
2025-06-11 19:19:53 -0400

    Code and CAD
    Split the top case for easier printing.
    Worked a bit on the video recording in digicam code.
    Lastly, playing around with tkinter and trying to create a usable app

--- Code/DigiCam.py
+++ Code/DigiCam.py
@@ -2,25 +2,42 @@ from time import strftime
 import pygame
 from gpiozero import Button
 from picamera2 import Picamera2, Preview
+from picamera2.encoders import H264Encoder
 
 
 
 cam = Picamera2()
 previewB = Button(14)
 photoB = Button(15)
+videoB = Button(18)
 
 #set preview parameters
 #previewPara = cam.create_preview_configuration(Preview.QT)
+video_config = cam.create_video_configuration()
+cam.configure(video_config)
+encoder = H264Encoder()
 
 
 turnedOn = True
+recording = False
 
 cam.start_preview(Preview.QTGL)
 cam.start(show_preview=True)
 
 
 def capture():
-    cam.capture_file('/home/krill/camera/{strftime("%Y%m%d-%H%M%S")}.jpg')
+    currentTime= strftime("%d-%m-%y-%I:%M:%S")
+    #make sure to change this to your wanted save location
+    cam.capture_file('/home/krill/camera/'+currentTime+'.jpg')
+def video_capture():
+    if(recording == True):
+        currentTime= strftime("%d-%m-%y-%I:%M:%S")
+        cam.start_recording(encoder, '/home/krill/camera/'+currentTime+'.h264')
+    elif(recording == False):
+        cam.stop_recording()
+
+
+
 
 while True:
     if previewB.is_pressed:
@@ -30,5 +47,11 @@ while True:
         elif(turnedOn == False):
             turnedOn = True
             cam.start_preview(Preview.QTGL)
-    if(turnedOn == True):
+    if videoB.is_pressed:
+        video_capture()
+        if(recording == True):
+            recording = False
+        elif(recording == False):
+            recording = True
+    if(turnedOn == True and recording == False):
         photoB.when_pressed = capture
--- Code/GUItest.py
+++ Code/GUItest.py
@@ -1,8 +1,15 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
+import ttkbootstrap as ttk
+import cv2
+from PIL import Image, ImageTk
 
-root = tk.Tk()
+root = ttk.Window(themename='journal')
 root.title("Krill Kam!")
+root.state('zoomed')
+
+
 
 capture = tk.Button
 
